a234fc9ca4ad318fe55aad10f0832707bdaf97dd
gabiyanu <78000716+gabiyanu@users.noreply.github.com>
2026-01-15 04:08:41 +0000

    delete all files.

--- src/animations/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Animation module for creating video/GIF content."""
-from src.animations.match_animation import MatchAnimator, GoalSequenceAnimator
--- src/export/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Export module for Instagram-ready content."""
-from src.export.instagram import InstagramExporter, create_instagram_post
